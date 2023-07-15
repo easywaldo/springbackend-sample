96455c063558e2a5fc125e5b4f24481238488fb3
easywaldo <easywaldoji@gmail.com>
2023-07-15 17:09:35 +0900

    유저 토큰으로 부터 사용자 이메일 정보를 가져올 수 있다

--- src/test/java/com/jwt/TokenProviderTest.java
+++ src/test/java/com/jwt/TokenProviderTest.java
@@ -1,5 +1,8 @@
 package com.jwt;
 
+import lombok.val;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import root.Application;
 import root.domain.User;
 import root.config.jwt.JwtProperties;
@@ -63,5 +66,22 @@ public class TokenProviderTest {
 
         // then
         assertThat(result).isFalse();
+    }
+
+    @DisplayName("getAuthentication 을 이용하여 토큰으로 뷰터 유저 정보를 가져올 수 있음")
+    @Test
+    void getAuthentication() {
+        // given
+        String userEmail = "acetious@gmail.com";
+        String token = JwtFactory.builder()
+                .subject(userEmail)
+                .build()
+                .createToken(jwtProperties);
+        // when
+        Authentication authentication = tokenProvider.getAuthentication(token);
+
+        // then
+        val userDetails = (UserDetails) authentication.getPrincipal();
+        assertThat(userDetails.getUsername()).isEqualTo(userEmail);
     }
 }
